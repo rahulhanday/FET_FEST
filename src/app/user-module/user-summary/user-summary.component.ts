70b6d04976d3d65f03a797e73282ab6aac0c8769
Rahul Handay <rahulhanday@gmail.com>
2019-06-15 13:27:41 +0530

    Add Code | User Summary

--- src/app/user-module/user-summary/user-summary.component.ts
+++ src/app/user-module/user-summary/user-summary.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { UserDataService } from '../user-data.service';
 
 @Component({
-    templateUrl: './user-summary.component.html'
+    templateUrl: './user-summary.component.html',
+    styleUrls: [
+        'user-summary.component.scss'
+    ],
+    encapsulation: ViewEncapsulation.None
 })
 export class UserSummaryComponent {
-
+    constructor(private userDataService: UserDataService) { }
 }
