0ad9855684b951bb5a53fe7f0efe6c07c4fa2547
Rahul Handay <rahulhanday@gmail.com>
2019-06-15 15:26:02 +0530

    Add Code | Documention

--- src/app/shared-module/services/context.service.ts
+++ src/app/shared-module/services/context.service.ts
@@ -10,15 +10,25 @@ export class ContextService {
     private context$ = new Subject<IContext>();
     constructor() { }
 
+    /**
+     * Get current context
+     */
     getContext(): IContext {
         return this.currentContext;
     }
 
+    /**
+     * Set current context
+     * param value
+     */
     setContext(value: IContext): void {
         this.currentContext = value;
         this.context$.next(value);
     }
 
+    /**
+     * Fire observable on context
+     */
     onContextChange() {
         return this.context$.asObservable();
     }
