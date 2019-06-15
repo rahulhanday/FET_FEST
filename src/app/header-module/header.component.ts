b895c165f6e87b2da314f6565959a5fbaa7502fc
Rahul Handay <rahulhanday@gmail.com>
2019-06-15 12:29:08 +0530

    Add Code | Header

--- src/app/header-module/header.component.ts
+++ src/app/header-module/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { LanguageService } from '../shared-module/services/language.service';
 import { LANGUAGE_CODES_ENUM } from '../shared-module/shared.constant';
+import { ContextService } from '../shared-module/services/context.service';
+import { IContext } from '../shared-module/shared.model';
 
 @Component({
     selector: 'header-component',
@@ -11,12 +13,21 @@ import { LANGUAGE_CODES_ENUM } from '../shared-module/shared.constant';
 export class HeaderComponent {
     currentLanguage: string;
     languageCodes: any;
-    constructor(private languageService: LanguageService) {
+    context: IContext;
+    constructor(private languageService: LanguageService, private contextService: ContextService) {
         this.currentLanguage = this.languageService.getLanguage();
         this.languageCodes = LANGUAGE_CODES_ENUM;
+        this.context = this.contextService.getContext();
+        this.init();
     }
     changeLanguage(languageCode: string) {
         this.currentLanguage = languageCode;
         this.languageService.setLanguage(this.currentLanguage);
+    }
+
+    private init() {
+        this.contextService.onContextChange().subscribe((context) => {
+            this.context = context;
+        });
     }
 }
