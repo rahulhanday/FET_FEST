ba6e04189d3da593f239dcb69b03700583714962
er-noor <hakimec17@gmail.com>
2019-06-15 14:41:53 +0530

    price for user product

--- src/app/user-module/user-product/user-product.component.ts
+++ src/app/user-module/user-product/user-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserProductService } from './user-product.service';
 import { IProduct } from '../user.model';
 import { UserDataService } from '../user-data.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './user-product.component.html',
@@ -9,9 +10,16 @@ import { UserDataService } from '../user-data.service';
 })
 export class UserProductComponent implements OnInit {
     userProductList: IProduct[];
+    totalAmount: number;
+    savedAmount: number;
+    noProductError: boolean;
     constructor(private userProductService: UserProductService,
-        private userDataService: UserDataService) {
+        private userDataService: UserDataService,
+        private router: Router) {
         this.userProductList = [];
+        this.totalAmount = 0;
+        this.savedAmount = 0;
+        this.noProductError = false;
     }
 
     /**on init interface function to initialize product component */
@@ -30,13 +38,35 @@ export class UserProductComponent implements OnInit {
         const product: IProduct = this.getProduct(id);
         const quantity = parseInt(product.quantity, 10);
         if (quantity && quantity > 0) {
+            this.noProductError = false;
             this.userDataService.addProductToCart(product);
             product.quantity = '';
         } else {
             product.error = true;
         }
+        this.calcalateAmount();
     }
 
+    /**
+     * calculate the amount user has to pay and saved
+     */
+    calcalateAmount() {
+        let tAmount = 0;
+        let sAmount = 0;
+        for (let i = 0; i < this.userDataService.userCartList.length; i++) {
+            tAmount += parseInt(this.userDataService.userCartList[i].quantity, 10) *
+                this.userDataService.userCartList[i].price;
+            sAmount = parseInt(this.userDataService.userCartList[i].quantity, 10) *
+                this.userDataService.userCartList[i].marketPrice;
+        }
+        this.totalAmount = tAmount;
+        this.savedAmount = sAmount - tAmount;
+    }
+
+    /**
+     * on every quantity change - this gets triggered
+     * param product - product user modified
+     */
     onQuantityChange(product: IProduct) {
         product.error = false;
     }
@@ -53,5 +83,17 @@ export class UserProductComponent implements OnInit {
             }
         }
         return product;
+    }
+
+
+    /**
+     * proceed to next route
+     */
+    onProceed() {
+        if (this.userDataService.userCartList.length <= 0) {
+            this.noProductError = true;
+            return;
+        }
+        this.router.navigate(['user/summary']);
     }
 }
